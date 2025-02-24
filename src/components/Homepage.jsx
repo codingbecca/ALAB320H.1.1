85b5b77b386be3024ee23f7a73f81449f1bdc3ec
codingbecca <131915898+codingbecca@users.noreply.github.com>
2025-02-24 12:25:04 -0800

    SearchBar component and general styling

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,18 @@
+import "./Homepage.css"
+
 import EmployeeList from "./EmployeeList";
 import Header from "./Header";
+import SearchBar from "./SearchBar";
 
 export default function Homepage() {
   return (
-    <>
+    <div className="homepage">
       <Header />
+      <SearchBar />
+      <div>
       <EmployeeList />
-    </>
+
+      </div>
+    </div>
   );
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,7 @@
+import "./SearchBar.css"
+
+export default function SearchBar () {
+    return (
+        <input type="text" name="search" id="search" />
+    )
+}
